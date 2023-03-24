3c3a06d975d5b3ec62e2b710f93abc8bc576c57f
Fabian Mikołajczak <fabian@mikolajczak.org>
2023-03-24 11:13:55 +0100

    First version which somehow works.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // constance definitions
-const waitForStart = 3
+const waitForStart = 1
+const questionsPerSet = 5
 
 
 // GUI element bindings
@@ -9,12 +10,14 @@ const okButton = document.querySelector('[data-ok]')
 const responseInput = document.getElementById('number_response')
 const questionError = document.querySelector('[data-question-error]')
 const rangeInput = document.getElementById('range')
+const questionLog = document.getElementById('response_log')
+const resultMessage = document.getElementById('result_message')
 
 function showErrorMessage(msg) {
     showMessage("<font color ='#ff0000'>" + msg + "</font>")
 }
 
-function showMessage(msg, div = document.getElementById('question')) {
+function showMessage(msg, div = questionDiv ) {
     div.innerHTML = msg
 }
 
@@ -31,19 +34,34 @@ function newQuestion(range) {
     return {
         varA: varA, 
         varB: varB,
-        answer: varA * varB
+        answer: varA * varB,
+        wrongAnswerCount: 0,
     }
 }
 
 let currentQuestion 
 let questionStack = []
+let questionLeft
 
-function startQuiz () {
+function showNextQuestion () {
+    questionLeft--
     responseInput.value = ''
     questionError.innerHTML = ''
     currentQuestion = newQuestion(range)
     showMessage(`${currentQuestion.varA} x ${currentQuestion.varB} = `)
+    responseInput.focus()
+    console.log(`questionLeft: ${questionLeft}`)
 }
+
+function updateQuestionLog(questionStack) {
+    questionLog.innerHTML =''
+    questionStack.forEach( passQuestion => {
+        questionLog.innerHTML = passQuestion.varA + ' x ' 
+        + passQuestion.varB + ' = ' + passQuestion.answer + '<br>' 
+        + questionLog.innerHTML
+    })
+}
+
 
 function okButtonListener() {
     // do nothing if there is no question
@@ -51,12 +69,33 @@ function okButtonListener() {
     // check if answer is correct
     if (responseInput.value != currentQuestion.answer) { 
         questionError.innerHTML = 'zła odpowiedź! spróbuj jeszcze raz!'
+        currentQuestion.wrongAnswerCount++
         return
     }
     // save time and stack question 
     questionStack.push(currentQuestion)
+    updateQuestionLog(questionStack)
     // generate and ask next question or end the test if there was enough questions
-    startQuiz()
+    if (questionLeft > 0) {
+        showNextQuestion()
+    } else {
+        clearQuestionPanel()
+        showSummary(questionStack)
+    }
+}
+
+function clearQuestionPanel() {
+    questionDiv.innerHTML = 'koniec testu!'
+}
+
+
+function showSummary(questionStack) {
+    let wrongAnswers = 0
+    questionStack.forEach( answer => {
+        if (answer.wrongAnswerCount > 0) wrongAnswers++
+    })
+    resultMessage.innerHTML = `Quiz ukończony! Dobre odpowiedzi, ` + 
+         `za pierwszym razem: ${questionStack.length - wrongAnswers}`
 }
 
 function countDown(counter, callback) { 
@@ -72,9 +111,9 @@ function start() {
         showErrorMessage('wrong A range')
         return
     }
-
+    questionLeft = questionsPerSet
     showMessage('game is starting! ;)')
-    countDown(waitForStart, startQuiz)
+    countDown(waitForStart, showNextQuestion)
     console.log(`waitForStart: ${waitForStart}`)
 }
 
